5e483ec70a065f4b073724315e9d2277aa8de108
Dikshita Sarkar <dikshitasarkar65@gmail.com>
2026-07-26 22:21:13 +0530

    Update generate_metric.py

--- src/generate_metric.py
+++ src/generate_metric.py
@@ -1,1 +1,10 @@
-
+from sklearn.metrics import (
+    accuracy_score,
+    classification_report,
+    confusion_matrix,
+    roc_auc_score
+)
+print("Accuracy:", accuracy_score(y_test, y_pred))
+print(classification_report(y_test, y_pred))
+print(confusion_matrix(y_test, y_pred))
+print("AUC:", roc_auc_score(y_test, y_prob))
