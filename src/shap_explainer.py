e5bbcfd4e56088206b1acc48b9e274df1ec14763
Dikshita Sarkar <dikshitasarkar65@gmail.com>
2026-07-26 23:07:08 +0530

    Update shap_explainer.py

--- src/shap_explainer.py
+++ src/shap_explainer.py
@@ -1,5 +1,20 @@
+import matplotlib.pyplot as plt
 import shap
+
+# Create SHAP Explainer
 explainer = shap.Explainer(rf_model, X_train)
+
+# Generate SHAP Values
 shap_values = explainer(X_test)
-shap.plots.beeswarm(shap_values[:, :, 1])
-shap.plots.waterfall(shap_values[0, :, 1])
+
+# SHAP Summary Plot
+plt.figure(figsize=(10,6))
+shap.plots.beeswarm(shap_values[:, :, 1], show=False)
+plt.savefig("../assets/shap_summary.png")
+plt.show()
+
+# SHAP Waterfall Plot
+plt.figure(figsize=(10,6))
+shap.plots.waterfall(shap_values[0, :, 1], show=False)
+plt.savefig("../assets/shap_waterfall.png")
+plt.show()
